f2e966d957833ece513c1b8cfb152fe0c7219310
Sirin <sirin@ants-macbook-pro.local>
2018-01-29 16:46:55 +0800

    Initial Commit

---
+++ iMessages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMessages
+//
+//  Created by Sirin on 29/01/2018.
+//  Copyright © 2018 Sirin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
